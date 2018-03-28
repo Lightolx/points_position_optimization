194da971cf2663c767502d6cf862f67412b10944
Xiao Lu <xiao.lu@ygomi.com>
2018-03-28 15:18:34 +0800

    No build error

--- kernel.h
+++ kernel.h
@@ -6,32 +6,57 @@
 #define INC_3D_POINT_FILTERING_KERNEL_H
 
 #include "Eigen/Eigen"
+#include <opencv2/opencv.hpp>
 
 class Kernel
 {
 public:
+    // Constructor
     Kernel() {}
-    Kernel(const Eigen::Vector3d &point, const double &radius): sPoint(point), h(radius) {}
+    Kernel(const Eigen::Vector3d &point, const double &radius): p_(point), h_(radius) {}
+
+    // Fill in the neighbor of the searching point
     void setNeighbors(const std::vector<Eigen::Vector3d> &points);
-    void computeNormal();
+
+    // Compute the covariance matrix
     void computeC();
+
+    // Compute the normal of the fitted plane
+    void computeNormal();
+
+    // Compute the Gaussian distribution parameter sigma_X and sigma_Y
     void computeFai();
 
+    // Compute the ellipsoid formed by this kernel, all paremeters included
+    void computeEllip();
+
+    // Monotonically decreasing weighted function
+    double kai(const Eigen::Vector3d &pt) const;
+
 public:
-    Eigen::Vector3d sPoint;  // searching point
-    double h;                // kernel size
-    std::vector<Eigen::Vector3d> neighbors;  // spatial neighborhood of searching point
-    Eigen::Vector3d cPoint;  // weighted center point
-    Eigen::Matrix3d C;       // weighted covariance matrix
-    Eigen::Vector3d v1, v2;  // orthogonal vectors that consist of the fitted plane
-    Eigen::Vector3d n;       // normal vector of the least_squared plane fitted to neighbors
+    Eigen::Vector3d p_;  // searching point
+    double h_;                // kernel size
+    std::vector<Eigen::Vector3d> neighbors_;  // spatial neighborhood of searching point
+    Eigen::Vector3d c_;  // weighted center point
+    Eigen::Vector3d v1_, v2_;  // orthogonal vectors that consist of the fitted plane
+    Eigen::Vector3d n_;       // normal vector of the least_squared plane fitted to neighbors
     double sigma_X, sigma_Y; // function fai which is a 2D gaussian distribution
 };
 
 void Kernel::setNeighbors(const std::vector<Eigen::Vector3d> &points)
 {
-    neighbors.clear();
-    neighbors.assign(points.begin(), points.end());
+    neighbors_.clear();
+    neighbors_.assign(points.begin(), points.end());
+}
+
+void Kernel::computeC()
+{
+//    C = Eigen::Matrix3d::Zero();
+//
+//    for (const Eigen::Vector3d &pt : neighbors)
+//    {
+//        C += kai(pt) * (pt - cPoint) * (pt - cPoint).inverse();
+//    }
 }
 
 void Kernel::computeNormal()
@@ -39,13 +64,90 @@ void Kernel::computeNormal()
 
 }
 
-void Kernel::computeC()
+void Kernel::computeFai()
 {
 
 }
 
-void Kernel::computeFai()
+double Kernel::kai(const Eigen::Vector3d &pt) const
 {
+    double dist = (pt - p_).norm();
+    return 1 - 0.2 * dist / h_;
+}
+
+void Kernel::computeEllip()
+{
+    //step1: get the weighted center c_
+    for (const Eigen::Vector3d &pt : neighbors_)
+    {
+        // todo:: there should be a weighted accumulate, not just a gravity center
+//        c_ += kai(pt) * pt;
+        c_ += pt;
+    }
+    c_ /= neighbors_.size();
+
+    // step2: compute the covariance matrix C
+    Eigen::Matrix3d C = Eigen::Matrix3d::Zero();
+    for (const Eigen::Vector3d &pt : neighbors_)
+    {
+        C += kai(pt) * (pt - c_) * (pt - c_).transpose();
+    }
+
+    // step3: eigenvalue decompose C to get the major and minor axis, the two vector form the
+    //        x0y plane, cross them to get normal n as axis z
+    Eigen::EigenSolver<Eigen::Matrix3d> es(C);
+    std::cout << C << std::endl;
+    Eigen::Matrix3d V = es.pseudoEigenvectors();
+    std::cout << std::endl << V << std::endl;
+    Eigen::Matrix3d D = es.pseudoEigenvalueMatrix();
+    std::cout << std::endl << D << std::endl;
+    std::vector<double> eigVals(3);
+    eigVals[0] = D(0, 0);
+    eigVals[1] = D(1, 1);
+    eigVals[2] = D(2, 2);
+
+    // return value of pseudoEigenValue() is unordered, so manually find the max and the mid one
+    std::vector<double>::iterator iter = std::max_element(eigVals.begin(), eigVals.end());
+    int maxId = std::distance(eigVals.begin(), iter);
+    iter = std::min_element(eigVals.begin(), eigVals.end());
+    int minId = std::distance(eigVals.begin(), iter);
+    int midId = 0;
+
+    for (int i = 0; i < 3; ++i)
+    {
+        if (i != maxId && i != minId)
+        {
+            midId = i;
+            break;
+        }
+    }
+
+    v1_ = V.col(maxId);
+    v2_ = V.col(midId);
+    n_ = v1_.cross(v2_);
+
+    // step4: project all neighbors to plane x0y, do a ellipse fitting to get the
+    //        Gaussian distribution parameters sigma_X and sigma_Y
+    std::vector<cv::Point2d> pts;
+    pts.reserve(neighbors_.size());
+    std::vector<double> Xs, Ys;
+    Xs.reserve(neighbors_.size());
+    Ys.reserve(neighbors_.size());
+
+    for (const Eigen::Vector3d &pt : neighbors_)
+    {
+        pts.push_back(cv::Point2d((pt - c_).dot(v1_), (pt - c_).dot(v2_)));
+        Xs.push_back(fabs((pt - c_).dot(v1_)));
+        Ys.push_back(fabs((pt - c_).dot(v2_)));
+    }
+
+    // todo:: there should be a ellipse fitting, first extract the boundary, then do cv::fitEllipse
+//    cv::RotatedRect rect = cv::fitEllipse(pts);
+    iter = std::max_element(Xs.begin(), Xs.end());
+    sigma_X = *iter / 3;
+    iter = std::max_element(Ys.begin(), Ys.end());
+    sigma_Y = *iter / 3;
+
 
 }
 
--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <fstream>
 
+#include <pcl/io/ply_io.h>
+#include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
-#include <pcl-1.7/pcl/registration/icp.h>
+#include <pcl/registration/icp.h>
 #include <Eigen/Eigen>
 #include <ceres/ceres.h>
 
@@ -13,26 +15,27 @@ typedef pcl::PointCloud<PointT> PointCloudT;
 
 struct SURFACE_FITTING_COST
 {
-    SURFACE_FITTING_COST(Eigen::Vector3d p, Eigen::Vector3d c, double h,
-                         Eigen::Vector3d v1, Eigen::Vector3d v2,
+    SURFACE_FITTING_COST(Eigen::Vector3d c, double h, Eigen::Vector3d v1, Eigen::Vector3d v2,
                          Eigen::Vector3d n, double sigmaX, double sigmaY): c_(c),
                          v1_(v1), v2_(v2), n_(n), h_(h), sigmaX_(sigmaX), sigmaY_(sigmaY)
             {}
 
-    bool operator()(const double* const position, double* residual) const
+    template <typename T>
+    bool operator()(const T* const position, T* residual) const
     {
-        Eigen::Vector3d p(position[0], position[1], position[2]);
-//        residual[0] = fai(p) * (pow(h_, 2) - (p - c_).dot(n_));
-        residual[0] = position[0] + position[1] + position[2];
+        Eigen::Matrix<T, 3, 1> p(position[0], position[1], position[2]);
+        residual[0] = fai(p) * (pow(h_, 2) - pow((p - c_).dot(n_), 2));
 
         return true;
     }
 
-    double fai(const Eigen::Vector3d &p) const
+    // Compute the coefficient simulated by a gaussian distribution
+    template <typename T>
+    T fai(const  Eigen::Matrix<T, 3, 1> &p) const
     {
-        double x = (p - c_).dot(v1_);
-        double y = (p - c_).dot(v2_);
-        double index = -0.5 * (pow(x, 2) / pow(sigmaX_, 2) + pow(y, 2) / pow(sigmaY_, 2));
+        T x = (p - c_).dot(v1_);
+        T y = (p - c_).dot(v2_);
+        T index = -0.5 * (pow(x, 2) / pow(sigmaX_, 2) + pow(y, 2) / pow(sigmaY_, 2));
         return 1 / (2 * M_PI * sigmaX_ * sigmaY_) * ceres::exp(index);
     }
 
@@ -60,7 +63,23 @@ int main()
         points.push_back(Eigen::Vector3d(x,y,z));
     }
 
-    // Step1: construct the neighborhood of every point pi, version 1: fixed kernel size
+    /*
+    PointCloudT::Ptr cloud(new PointCloudT);
+//    if (pcl::io::loadPLYFile("dragon.ply", *cloud) < 0)
+//    {
+//        std::cerr << "no ply file found!" << std::endl;
+//        abort();
+//    }
+
+    if (pcl::io::loadPCDFile("office.pcd", *cloud) < 0)
+    {
+        std::cerr << "no ply file found!" << std::endl;
+        abort();
+    }
+     */
+
+    // Step1: For every point pi, Construct a kernel including it and its neighborhood, version 1: fixed kernel size
+    // step1.1: find pi's neighbor
     PointCloudT::Ptr cloud(new PointCloudT);
     cloud->width = points.size();
     cloud->height = 1;
@@ -74,6 +93,16 @@ int main()
         cloud->points[i].z = points[i].z();
     }
 
+    /*
+    std::vector<Eigen::Vector3d> points;
+    points.reserve(cloud->size());
+    for (int i = 0; i < cloud->size(); i++)
+    {
+        points[i].x() = cloud->points[i].x;
+        points[i].y() = cloud->points[i].y;
+        points[i].z() = cloud->points[i].z;
+    }*/
+
     pcl::search::KdTree<PointT>::Ptr kdTree(new pcl::search::KdTree<PointT>);
     kdTree->setInputCloud(cloud);
     float h = 0.2;                // kernel size
@@ -83,18 +112,17 @@ int main()
     std::vector<Kernel> kernels;
     kernels.reserve(cloud->size());
 
+    // step1.2: packaging pi and its neighbor as a kernel
     for (int i = 0; i < cloud->size(); i++)
     {
-        PointT sPoint;
+        PointT sPoint = cloud->points[i];
 
-        if (kdTree->radiusSearchT(sPoint, h, pointsID, pointsSquaredDist) > 0)
+        if (kdTree->radiusSearchT(sPoint, h, pointsID, pointsSquaredDist) > 1)
         {
-    // Step2: Compute the normal of the least_squared plane fitted to neighbor pi
-
-            // step2.1: set center and size
+            // set center and size
             Kernel kernel(points[i], h);
 
-            // step2.2: set neighbors
+            // set neighbors
             neighbors.clear();
             for (const int &id : pointsID)
             {
@@ -102,46 +130,86 @@ int main()
             }
             kernel.setNeighbors(neighbors);
 
-            // step2.3: store this kernel
+            // store this kernel
             kernels.push_back(kernel);
         }
     }
 
+    // Step2: Compute all parameter of the kernels, including v1_, v2_ and n_
+    // Step2: Compute the normal of the least_squared plane fitted to neighbor pi
     // Step3: Compute the monotonically decreasing weighting function of every piece pi
     for (Kernel &kernel : kernels)
     {
-        // step3.1: compute normal of the fitted plane
-        kernel.computeNormal();
-
-        // step3.2: compute the weighted covariance matrix C
-        kernel.computeC();
-
-        // step3.3: compute the Gaussian function fai
-        kernel.computeFai();
+        kernel.computeEllip();
+//        // step3.1: compute the weighted covariance matrix C
+//        kernel.computeC();
+//
+//        // step3.2: compute normal of the fitted plane
+//        kernel.computeNormal();
+//
+//        // step3.3: compute the Gaussian function fai
+//        kernel.computeFai();
     }
 
-    // Step4: Accumulate all likelihood function to get the cost function
-    double xyz[3] = {points[0][0], points[0][1], points[0][2]};
-    Eigen::Vector3d cPoint(points[0]);
-    ceres::Problem problem;
-    for (const Kernel &kernel : kernels)
+    // Step4: Accumulate all likelihood function to get the cost function, then optimize all
+    //        points' position
+    int i = 0;
+    for (Eigen::Vector3d &point : points)
     {
-        Eigen::Vector3d p = kernel.sPoint;
-        Eigen::Vector3d c = kernel.cPoint;
-        Eigen::Vector3d v1 = kernel.v1;
-        Eigen::Vector3d v2 = kernel.v2;
-        Eigen::Vector3d n = kernel.n;
-        double sigma_X = kernel.sigma_X;
-        double sigma_Y = kernel.sigma_Y;
-        problem.AddResidualBlock(new ceres::AutoDiffCostFunction<SURFACE_FITTING_COST, 1, 3>
-        (new SURFACE_FITTING_COST(p, c, h, v1, v2, n, sigma_X, sigma_Y)
-        ), nullptr, xyz);
+        double xyz[3] = {point[0], point[1], point[2]};
+        ceres::Problem problem;
+
+        // add up all kernel's influence as residual blocks
+        for (const Kernel &kernel : kernels)
+        {
+            Eigen::Vector3d c = kernel.c_;
+            Eigen::Vector3d v1 = kernel.v1_;
+            Eigen::Vector3d v2 = kernel.v2_;
+            Eigen::Vector3d n = kernel.n_;
+            double sigma_X = kernel.sigma_X;
+            double sigma_Y = kernel.sigma_Y;
+
+            if ((point - c).norm() > 5 * h)
+            {
+                continue;
+            }
+
+            // *******************************debug*********************************//
+            double len1 = v1.norm();
+            double len2 = v2.norm();
+            double len3 = n.norm();
+            double x = (point - c).dot(v1);
+            double y = (point - c).dot(v2);
+            double index = -0.5 * (pow(x, 2) / pow(sigma_X, 2) + pow(y, 2) / pow(sigma_Y, 2));
+            double fai = 1 / (2 * M_PI * sigma_X * sigma_Y) * ceres::exp(index);
+            double dist = (point - c).dot(n);
+            double residual = fai * (pow(h, 2) - pow((point - c).dot(n), 2));
+            // *******************************debug*********************************//
+
+            problem.AddResidualBlock(new ceres::AutoDiffCostFunction<SURFACE_FITTING_COST, 1, 3>
+                                    (new SURFACE_FITTING_COST(c, h, v1, v2, n, sigma_X, sigma_Y)),
+                                     nullptr, xyz);
+        }
+
+        ceres::Solver::Options options;
+        options.linear_solver_type = ceres::DENSE_NORMAL_CHOLESKY;
+
+        ceres::Solver::Summary summary;
+        ceres::Solve(options, &problem, &summary);
+
+        std::cout << "A point optimize done, i = " << i++ << std::endl;
+
+        // write back optimization results
+        point = Eigen::Vector3d(xyz[0], xyz[1], xyz[2]);
     }
 
-    ceres::Solver::Options options;
-    options.linear_solver_type = ceres::DENSE_NORMAL_CHOLESKY;
+    // Step5: Write back 3D points and export it
+    for (int i = 0; i < cloud->size(); i++)
+    {
+        cloud->points[i].x = points[i].x();
+        cloud->points[i].y = points[i].y();
+        cloud->points[i].z = points[i].z();
+    }
 
-    ceres::Solver::Summary summary;
-    ceres::Solve(options, &problem, &summary);
-
+    pcl::io::savePLYFile("opt.ply", *cloud);
 }
